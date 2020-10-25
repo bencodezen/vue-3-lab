37f08d8c3ccc46fd16bcdc525e67098e1e2513be
Ben Hong <ben@bencodezen.io>
2020-10-24 20:57:40 -0400

    test: add test for udpating timeframe

--- src/components/Timeline.spec.ts
+++ src/components/Timeline.spec.ts
@@ -9,4 +9,25 @@ describe('Timeline', () => {
 
     expect(periods).toHaveLength(3)
   })
+
+  it('updates timeframe when clicked', async () => {
+    const wrapper = mount(Timeline)
+
+    const $today = wrapper.findAll('button')[0]
+    const $week = wrapper.findAll('button')[1]
+    const $month = wrapper.findAll('button')[2]
+    const $pageHeading = wrapper.findAll('h1')[0]
+
+    // Default state
+    expect($pageHeading.text()).toContain('Today')
+
+    await $week.trigger('click')
+    expect($pageHeading.text()).toContain('This Week')
+
+    await $month.trigger('click')
+    expect($pageHeading.text()).toContain('This Month')
+
+    await $today.trigger('click')
+    expect($pageHeading.text()).toContain('Today')
+  })
 })
