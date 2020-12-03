07f4e1935323bce97d957adcc2c13fd8ce00773a
Ben Hong <ben@bencodezen.io>
2020-12-03 16:41:32 -0500

    update dependencies

--- js/src/views/Home.vue
+++ js/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="fetchBackgroundFunction">Fetch Background Fn</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    fetchBackgroundFunction() {
+      axios
+        .post('/.netlify/functions/hello-background', { my: 'payload' })
+        .then(response => {
+          console.log(response)
+        })
+    }
   }
 }
 </script>
