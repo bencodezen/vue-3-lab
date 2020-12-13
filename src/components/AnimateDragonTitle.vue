52b14f206538aeff3ffaef8d573b019b815ede91
Ben Hong <ben@bencodezen.io>
2020-12-12 22:50:23 -0500

    refactor: abstract out animate dragon title

---
+++ src/components/AnimateDragonTitle.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  data: () => ({
+    isDragon: true
+  }),
+  computed: {
+    displayDragonStyles() {
+      return this.isDragon ? 'is-visible' : ''
+    },
+    displayUnicornStyles() {
+      return this.isDragon ? '' : 'is-visible'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="animate-dragon-title">
+    <div class="creature-cage">
+      <transition name="jiggle">
+        <h1 v-if="isDragon" class="creature">
+          Dragon 🐉
+        </h1>
+      </transition>
+    </div>
+    <button @click="isDragon = !isDragon">Toggle Dragon</button>
+  </div>
+</template>
+
+<style>
+.creature-cage {
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  height: 86px;
+}
+
+@keyframes jiggle {
+  0% {
+    transform: scale(0);
+  }
+  60% {
+    transform: scale(2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.creature:hover {
+  animation: jiggle 0.5s;
+  animation-fill-mode: forwards;
+}
+
+.jiggle-enter-active {
+  animation: jiggle 1s;
+}
+
+.jiggle-leave-active {
+  animation: jiggle 1s reverse;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -16,57 +16,8 @@ export default {
 
 <template>
   <div class="about">
-    <div class="creature-cage">
-      <transition name="jiggle">
-        <h1 v-if="isDragon" class="creature">
-          Dragon 🐉
-        </h1>
-      </transition>
-    </div>
-    <button @click="isDragon = !isDragon">Toggle Dragon</button>
+    <h1>About</h1>
   </div>
 </template>
 
-<style>
-.creature-cage {
-  position: relative;
-  display: block;
-  margin: 0 auto;
-  height: 86px;
-}
-
-@keyframes jiggle {
-  0% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(2);
-  }
-}
-
-.creature:hover {
-  animation: jiggle 0.5s;
-  animation-fill-mode: forwards;
-}
-
-.jiggle-enter-active {
-  animation: jiggle 1s;
-}
-
-.jiggle-leave-active {
-  animation: jiggle 1s reverse;
-}
-
-a {
-  border: 2px solid darkblue;
-  background-color: white;
-  color: darkblue;
-  text-decoration: none;
-  transition: background-color 0.2s ease-in, color 0.2s ease-in;
-}
-
-a:hover {
-  background-color: darkblue;
-  color: white;
-}
-</style>
+<style></style>
