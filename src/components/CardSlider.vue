fe8e73cbfee197ef52a1d989c75750f201305074
Ben Hong <ben@bencodezen.io>
2020-12-19 17:36:00 -0500

    look into card sliders

--- src/components/CardSlider.vue
+++ src/components/CardSlider.vue
@@ -1,61 +1,25 @@
 <script>
 export default {
-  mounted() {
-    const initCodepenJs = function() {
-      var carousel = document.getElementsByClassName('carousel')[0],
-        slider = carousel.getElementsByClassName('carousel__slider')[0],
-        items = carousel.getElementsByClassName('carousel__slider__item'),
-        prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
-        nextBtn = carousel.getElementsByClassName('carousel__next')[0]
+  data: () => ({
+    cardList: [1, 2, 3, 4, 5],
+    currIndex: 0
+  }),
+  methods: {
+    move(index) {
+      if (index < 1) index = this.cardList.length
+      if (index > this.cardList.length) index = 1
+      this.currIndex = index
 
-      var width,
-        height,
-        totalWidth,
-        margin = 20,
-        currIndex = 0,
-        interval,
-        intervalTime = 4000
-
-      function init() {
-        resize()
-        move(Math.floor(items.length / 2))
-        bindEvents()
-
-        timer()
-      }
-
-      function resize() {
-        width = Math.max(window.innerWidth * 0.25, 275)
-        height = window.innerHeight * 0.5
-        totalWidth = width * items.length
-
-        slider.style.width = totalWidth + 'px'
-
-        for (var i = 0; i < items.length; i++) {
-          let item = items[i]
-          item.style.width = width - margin * 2 + 'px'
-          item.style.height = height + 'px'
-        }
-      }
-
-      function move(index) {
-        if (index < 1) index = items.length
-        if (index > items.length) index = 1
-        currIndex = index
-
-        for (var i = 0; i < items.length; i++) {
-          let item = items[i],
-            box = item.getElementsByClassName('item__3d-frame')[0]
-          if (i == index - 1) {
-            item.classList.add('carousel__slider__item--active')
-            box.style.transform = 'perspective(1200px)'
-          } else {
-            item.classList.remove('carousel__slider__item--active')
-            box.style.transform =
-              'perspective(1200px) rotateY(' +
-              (i < index - 1 ? 40 : -40) +
-              'deg)'
-          }
+      for (var i = 0; i < this.cardList.length; i++) {
+        let item = this.cardList[i],
+          box = item.getElementsByClassName('item__3d-frame')[0]
+        if (i == index - 1) {
+          item.classList.add('carousel__slider__item--active')
+          box.style.transform = 'perspective(1200px)'
+        } else {
+          item.classList.remove('carousel__slider__item--active')
+          box.style.transform =
+            'perspective(1200px) rotateY(' + (i < index - 1 ? 40 : -40) + 'deg)'
         }
 
         slider.style.transform =
@@ -63,22 +27,46 @@ export default {
           (index * -width + width / 2 + window.innerWidth / 2) +
           'px, 0, 0)'
       }
+    }
+  },
+  mounted() {
+    const initCodepenJs = function() {
+      var carousel = document.getElementsByClassName('carousel')[0],
+        slider = carousel.getElementsByClassName('carousel__slider')[0],
+        prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+        nextBtn = carousel.getElementsByClassName('carousel__next')[0]
 
-      function timer() {
-        clearInterval(interval)
-        interval = setInterval(() => {
-          move(++currIndex)
-        }, intervalTime)
+      var width,
+        height,
+        // totalWidth,
+        margin = 20
+
+      function init() {
+        resize()
+        this.move(Math.floor(this.cardList.length / 2))
+        bindEvents()
+      }
+
+      function resize() {
+        width = Math.max(window.innerWidth * 0.25, 275)
+        height = window.innerHeight * 0.5
+        // totalWidth = width * this.cardList.length
+
+        // slider.style.width = totalWidth + 'px'
+
+        for (var i = 0; i < this.cardList.length; i++) {
+          let item = this.cardList[i]
+          item.style.width = width - margin * 2 + 'px'
+          item.style.height = height + 'px'
+        }
       }
 
       function prev() {
-        move(--currIndex)
-        timer()
+        this.move(--this.currIndex)
       }
 
       function next() {
-        move(++currIndex)
-        timer()
+        this.move(++this.currIndex)
       }
 
       function bindEvents() {
@@ -110,15 +98,15 @@ export default {
   <div class="carousel">
     <div class="carousel__body">
       <div class="carousel__prev">
-        <i class="far fa-angle-left"></i>
+        <p>Previous</p>
       </div>
       <div class="carousel__next">
-        <i class="far fa-angle-right"></i>
+        <p>Next</p>
       </div>
       <div class="carousel__slider">
         <div
           class="carousel__slider__item"
-          v-for="item in [1, 2, 3, 4, 5]"
+          v-for="item in cardList"
           :key="`card-${item}`"
         >
           <div class="item__3d-frame">
