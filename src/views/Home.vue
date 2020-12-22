d2237beaa8115bc75299479be418072accef543f
Ben Hong <ben@bencodezen.io>
2020-12-22 08:00:16 -0500

    feat: turn on bg functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,15 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <p>Count: {{ list }}</p>
-    <p>New List: {{ newList }}</p>
-    <button @click="list.push(5)">Add</button>
-    <button @click="newList.push(4)">New Add</button>
-  </div>
-</template>
-
 <script>
-import { reactive, ref, toRefs, watch } from 'vue'
+import BackgroundFunction from '../components/BackgroundFunction.vue'
 
 export default {
-  name: 'Home',
-  setup() {
-    const state = reactive({
-      list: [1, 2, 3]
-    })
-
-    const newList = ref([1, 2, 3])
-
-    watch(
-      () => [...state.list],
-      (currentValue, oldValue) => {
-        console.log(currentValue)
-        console.log(oldValue)
-      }
-    )
-
-    watch(
-      () => [...newList.value],
-      (currentValue, oldValue) => {
-        console.log('new', currentValue)
-        console.log('old', oldValue)
-      }
-    )
-
-    return {
-      ...toRefs(state),
-      newList
-    }
-  },
-  watch: {
-    list(currentValue, oldValue) {
-      console.log(currentValue)
-      console.log(oldValue)
-    }
+  components: {
+    BackgroundFunction
   }
 }
 </script>
+
+<template>
+  <background-function />
+</template>
+
+<style></style>
